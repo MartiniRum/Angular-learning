cd47c044e10a1b033add297249e054429d6ad132
MartiniRum <martiniromas@gmail.com>
2022-10-20 09:08:04 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { KmiComponent } from './components/kmi/kmi.component';
+import {FormsModule} from "@angular/forms";
+import { ListComponent } from './components/list/list.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KmiComponent,
+    ListComponent,
+    AddTaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
