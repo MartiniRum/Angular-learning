ac3e327efd49a7b1c925b7bd4eaf958576ae1ed8
MartiniRum <martiniromas@gmail.com>
2022-10-21 11:52:52 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { KmiComponent } from './components/kmi/kmi.component';
 import {FormsModule} from "@angular/forms";
 import { ListComponent } from './components/list/list.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import {RouterModule, Routes} from "@angular/router";
+import { EditTaskComponent } from './components/edit-task/edit-task.component';
+
+const appRoutes:Routes=[
+  {path:"", component:ListComponent},
+  {path:"new", component:AddTaskComponent},
+  {path:"edit/:id", component:EditTaskComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    KmiComponent,
     ListComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    EditTaskComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
